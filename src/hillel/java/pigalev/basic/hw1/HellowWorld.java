91391368f7d792197a74e253dca4af36207d4556
Danya <torinpigalev@gmai.com>
2025-05-26 23:31:12 +0300

    test2

--- src/hillel/java/pigalev/basic/hw1/HellowWorld.java
+++ src/hillel/java/pigalev/basic/hw1/HellowWorld.java
@@ -3,7 +3,8 @@ package hillel.java.pigalev.basic.hw1;
 public class HellowWorld {
     public static void main(String[] args) {
 
-        System.out.println("HellowWorld");
+        System.out.print("Hellow" );
+        System.out.println("World");
 
     }
 }
