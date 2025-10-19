1449d4a2613e6a32f9e978f4dd6484890c3056bb
Danya <torinpigalev@gmai.com>
2025-10-19 23:37:29 +0300

    test3

--- src/hillel/java/pigalev/basic/hw2/coordinatNumber.java
+++ src/hillel/java/pigalev/basic/hw2/coordinatNumber.java
@@ -3,9 +3,10 @@ package hillel.java.pigalev.basic.hw2;
 public class coordinatNumber {
     public static void main(String[] args) {
         System.out.println("My coordinat:");
-        double a = 46.48128118778591;
+        double a = 46.48;
         double b = 30.747039657671884;
         System.out.println("Latitude: " + a);
         System.out.println("Longitude: " + b);
     }
 }
+
